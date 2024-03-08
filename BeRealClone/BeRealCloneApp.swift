ae92e09eb832d76220e4de19e417dcff577c6dca
Elias0127 <eliasmekonen001@gmail.com>
2024-03-08 13:24:04 -0800

    Initial Commit

---
+++ BeRealClone/BeRealCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealCloneApp.swift
+//  BeRealClone
+//
+//  Created by Elias Woldie on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BeRealCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
