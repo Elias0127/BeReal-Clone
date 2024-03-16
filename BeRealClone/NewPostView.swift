64924281ce043f6fca8250c824e511e8307da9b1
Elias0127 <eliasmekonen001@gmail.com>
2024-03-15 22:53:51 -0700

    Added comment feature

--- BeRealClone/NewPostView.swift
+++ BeRealClone/NewPostView.swift
@@ -9,49 +9,107 @@ import Parse
 import SwiftUI
 
 struct Post: Identifiable {
-  var id: String
-  var image: UIImage?
-  var caption: String
-  var author: String
-  var location: String?
-  var timestamp: Date?
+    var id: String
+    var image: UIImage?
+    var caption: String
+    var author: String
+    var location: String?
+    var timestamp: Date?
+    var comments: [Comment] = []
 
 }
+struct Comment: Identifiable {
+    var id: String
+    var text: String
+    var author: String
+}
+
 
 struct PostView: View {
-  var post: Post
+    @Binding var post: Post
+    @State private var newCommentText: String = ""
+    @State private var isPostingComment: Bool = false
 
-  var body: some View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            // Display post content
+            if let image = post.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
+            Text(post.caption)
+                .font(.caption)
+                .foregroundColor(.gray)
+            Text("Posted by \(post.author)")
+                .font(.caption)
+                .foregroundColor(.secondary)
+            if let location = post.location {
+                Text(location)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+            if let timestamp = post.timestamp {
+                Text(timestamp, style: .date)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
 
-    VStack(alignment: .leading) {
-      if let image = post.image {
-        Image(uiImage: image)
-          .resizable()
-          .scaledToFit()
-      }
-      Text(post.caption)
-        .font(.caption)
-        .foregroundColor(.gray)
-      Text("Posted by \(post.author)")
-        .font(.caption)
-        .foregroundColor(.secondary)
-      if let location = post.location {
-        Text(location)
-          .font(.caption)
-          .foregroundColor(.secondary)
-      }
+            // Display comments
+            ForEach(post.comments) { comment in
+                HStack {
+                    Text(comment.author)
+                        .fontWeight(.bold)
+                    Text(comment.text)
+                }
+            }
 
-      if let timestamp = post.timestamp {
-        Text(timestamp, style: .date)
-          .font(.caption)
-          .foregroundColor(.secondary)
-      }
+            // Comment input field
+            HStack {
+                TextField("Add a comment...", text: $newCommentText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                if isPostingComment {
+                    ProgressView()
+                } else {
+                    Button("Post") {
+                        postComment()
+                    }
+                }
+            }
+        }
+        .background(Color.black)
+        .padding()
     }
-    .background(Color.black)
-    .padding()
 
-  }
+    func postComment() {
+        guard !newCommentText.isEmpty else { return }
+        isPostingComment = true
+
+        // Create a new Comment object
+        let comment = PFObject(className: "Comment")
+        comment["text"] = newCommentText
+        comment["post"] = PFObject(withoutDataWithClassName: "Post", objectId: post.id)
+        comment["author"] = PFUser.current()
+
+        // Save the comment to Parse
+        comment.saveInBackground { (success, error) in
+            DispatchQueue.main.async {
+                self.isPostingComment = false
+                if success {
+                    print("Comment posted successfully")
+                    // Add the new comment to the post's comments array
+                    let newComment = Comment(id: comment.objectId ?? "unknown", text: self.newCommentText, author: PFUser.current()?.username ?? "Anonymous")
+                    self.post.comments.append(newComment)
+                    self.newCommentText = "" // Clear the input field
+                } else if let error = error {
+                    print("Error posting comment: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
 }
+
 
 struct NewPostView: View {
   @State private var posts: [Post] = []
@@ -90,7 +148,7 @@ struct NewPostView: View {
       .padding()
 
       // List of posts
-      List(posts) { post in
+        List($posts) { post in
         if hasUploadedPost {
           PostView(post: post)
             .listRowBackground(Color.black)
@@ -109,59 +167,86 @@ struct NewPostView: View {
     }
     .background(Color.black.edgesIgnoringSafeArea(.all))
     .sheet(isPresented: $showingUploadPostView) {
-      UploadPostView(
-        isPresented: $showingUploadPostView,
-        didCompleteUpload: {
-          self.fetchPosts()  // This will refresh the posts list
+        UploadPostView(isPresented: $showingUploadPostView, didCompleteUpload: {
+            self.fetchPosts() // Refresh the posts list
+            self.hasUploadedPost = true // Update the state
         })
     }
-  }
+    .onAppear {
+        fetchUserData()
+        fetchPosts()
+    }
+}
 
-  func fetchPosts() {
+func fetchUserData() {
+    // Fetch the current user's data, including the hasUploadedPost flag
+    guard let currentUser = PFUser.current() else { return }
+    hasUploadedPost = currentUser["hasUploadedPost"] as? Bool ?? false
+}
+
+
+func fetchPosts() {
     let query = PFQuery(className: "Post")
     query.includeKey("author")
-    query.whereKey(
-      "createdAt",
-      greaterThanOrEqualTo: Calendar.current.date(byAdding: .day, value: -1, to: Date())!)
+    query.whereKey("createdAt", greaterThanOrEqualTo: Calendar.current.date(byAdding: .day, value: -1, to: Date())!)
     query.order(byDescending: "createdAt")
     query.limit = 10
 
     query.findObjectsInBackground { (objects, error) in
-      if let error = error {
-        print("Error fetching posts: \(error.localizedDescription)")
-      } else if let objects = objects {
-        var fetchedPosts: [Post] = []
+        if let error = error {
+            print("Error fetching posts: \(error.localizedDescription)")
+        } else if let objects = objects {
+            var fetchedPosts: [Post] = []
 
-        for object in objects {
-          if let author = object["author"] as? PFUser,
-            let file = object["image"] as? PFFileObject,
-            let caption = object["caption"] as? String
-          {
-            let postId = object.objectId ?? "unknown"
-            let authorName = author.username ?? "Anonymous"
-            let location = object["location"] as? String
-            let timestamp = object.createdAt
+            for object in objects {
+                if let author = object["author"] as? PFUser,
+                   let file = object["image"] as? PFFileObject,
+                   let caption = object["caption"] as? String {
+                    let postId = object.objectId ?? "unknown"
+                    let authorName = author.username ?? "Anonymous"
+                    let location = object["location"] as? String
+                    let timestamp = object.createdAt
 
-            file.getDataInBackground { (data, error) in
-              if let data = data, let image = UIImage(data: data) {
-                let post = Post(
-                  id: postId, image: image, caption: caption, author: authorName,
-                  location: location, timestamp: timestamp)
-                fetchedPosts.append(post)
+                    // Fetch comments for this post
+                    let commentQuery = PFQuery(className: "Comment")
+                    commentQuery.whereKey("post", equalTo: object)
+                    commentQuery.includeKey("author")
+                    commentQuery.findObjectsInBackground { (comments, error) in
+                        var fetchedComments: [Comment] = []
 
-                // Update the posts array on the main thread
-                DispatchQueue.main.async {
-                  self.posts = fetchedPosts
+                        if let comments = comments {
+                            for comment in comments {
+                                if let author = comment["author"] as? PFUser,
+                                   let text = comment["text"] as? String {
+                                    let commentId = comment.objectId ?? "unknown"
+                                    let authorName = author.username ?? "Anonymous"
+
+                                    let fetchedComment = Comment(id: commentId, text: text, author: authorName)
+                                    fetchedComments.append(fetchedComment)
+                                }
+                            }
+                        }
+
+                        file.getDataInBackground { (data, error) in
+                            if let data = data, let image = UIImage(data: data) {
+                                let post = Post(id: postId, image: image, caption: caption, author: authorName, location: location, timestamp: timestamp, comments: fetchedComments)
+                                fetchedPosts.append(post)
+
+                                // Update the posts array on the main thread
+                                DispatchQueue.main.async {
+                                    self.posts = fetchedPosts
+                                }
+                            } else if let error = error {
+                                print("Error fetching image for post: \(error.localizedDescription)")
+                            }
+                        }
+                    }
                 }
-              } else if let error = error {
-                print("Error fetching image for post: \(error.localizedDescription)")
-              }
             }
-          }
         }
-      }
     }
-  }
+}
+
 
   func fetchUserLastPostTimestamp(completion: @escaping (Date?) -> Void) {
     guard let currentUser = PFUser.current() else {
